78955e7f937e4a2b9f7f890371e9583d2ecf1299
F.S. Koulen <feryllia.koulen@hva.nl>
2023-09-14 14:22:05 +0200

    Implement deleting an appointment

--- web/delete.php
+++ web/delete.php
@@ -1,1 +1,14 @@
-<?php
+<?php include_once("config.php");
+// Get the id from the GET request
+$id = $_GET['id'];
+
+// Delete the record from the database
+$sql = "DELETE FROM APPOINTMENT WHERE id = '$id'";
+$result = mysqli_query($db, $sql);
+
+// Check if the deletion was successful. If successful, redirect to index.php
+if ($result) {
+    header("Location: index.php");
+} else { // If unsuccessful, display an error message
+    echo "Error: " . $sql . " " . mysqli_error($db);
+}
