4638a19f928ce5f07b2b250c94150d1122893955
F.S. Koulen <feryllia.koulen@hva.nl>
2023-10-01 22:50:12 +0200

    Move servo motor

---
+++ embedded/move-servo/move-servo.cpp
@@ -0,0 +1,29 @@
+/*
+ * Move a servo motor from 0 to 180 degrees and back.
+ *
+ * @author F.S. Koulen
+ * @date 2023-09-30
+ */
+
+#include <Arduino.h>
+#include <Servo.h>
+
+#define SERVO_PIN 4
+
+Servo motor;
+
+void setup() {
+    motor.attach(SERVO_PIN);
+}
+
+void loop() {
+    for (int angle = 0; angle <= 180; angle++) {
+        motor.write(angle);
+        delay(15);
+    }
+
+    for (int angle = 180; angle >= 0; angle--) {
+        motor.write(angle);
+        delay(15);
+    }
+}
