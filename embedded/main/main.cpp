4b6ba8c22903bf0cb7a83352a6a06e618a78902c
F.S. Koulen <feryllia.koulen@hva.nl>
2023-10-03 09:49:44 +0200

    Add logic to go back to home screen after a bit

--- embedded/main/main.cpp
+++ embedded/main/main.cpp
@@ -17,27 +17,28 @@ enum DisplayState {
     HYGROMETER
 };
 
-const int NUMBER_OF_DISPLAYS = sizeof(DisplayState) / sizeof(int) + 1;
-
 DisplayState currentDisplayState = TIME;
+
+const int NUMBER_OF_DISPLAYS = sizeof(DisplayState) / sizeof(int) + 1;
 unsigned long lastDisplayUpdateTime = 0;
+unsigned long lastButtonPressTime = 0;
+unsigned long timeToGoBackToTimeDisplay = 60 * 1000;
 
-/*
- * Initialize the screen and set the screen for the other components
+/**
+ * Get the display state as a string
+ *
+ * @param displayState The display state
+ * @return The display state as a string
  */
-void initializeScreen() {
-    screen.initializeLCD();
-    wifiManager.setScreen(screen);
-    timeManager.setScreen(screen);
-    hygrometer.setScreen(screen);
-}
-
-void setup() {
-    pinMode(NEXT_BUTTON_PIN, INPUT_PULLUP);
-    Serial.begin(SERIAL_BAUD_RATE);
-    Serial.println(); // Print empty line to separate boot messages from the rest of the output
-    initializeScreen();
-    wifiManager.initializeWiFi(WIFI_SSID, WIFI_PASSWORD);
+String getDisplayStateString(DisplayState displayState) {
+    switch (displayState) {
+        case TIME:
+            return "Time";
+        case HYGROMETER:
+            return "Hygrometer";
+        default:
+            return "Time";
+    }
 }
 
 /**
@@ -79,25 +80,36 @@ void displayContent() {
 void switchDisplayState() {
     // Increment the display mode (cycle through the available modes)
     currentDisplayState = static_cast<DisplayState>((currentDisplayState + 1) % NUMBER_OF_DISPLAYS);
+    Serial.println("Switched to display mode: " + getDisplayStateString(currentDisplayState));
+}
 
-    String currentDisplayStateString;
+/*
+ * Initialize the screen and set the screen for the other components
+ */
+void initializeScreen() {
+    screen.initializeLCD();
+    wifiManager.setScreen(screen);
+    timeManager.setScreen(screen);
+    hygrometer.setScreen(screen);
+}
 
-    switch (currentDisplayState) {
-        case TIME:
-            currentDisplayStateString = "Time";
-            break;
-        case HYGROMETER:
-            currentDisplayStateString = "Hygrometer";
-            break;
-        default:
-            currentDisplayStateString = "Time";
-    }
-
-    Serial.println("Switched to display mode: " + currentDisplayStateString);
+void setup() {
+    pinMode(NEXT_BUTTON_PIN, INPUT_PULLUP);
+    Serial.begin(SERIAL_BAUD_RATE);
+    Serial.println(); // Print empty line to separate boot messages from the rest of the output
+    initializeScreen();
+    wifiManager.initializeWiFi(WIFI_SSID, WIFI_PASSWORD);
 }
 
 void loop() {
     unsigned long currentTime = millis();
+
+    // Go back to home screen after a certain amount of time
+    if (currentDisplayState != TIME && currentTime - lastButtonPressTime >= timeToGoBackToTimeDisplay) {
+        currentDisplayState = TIME;
+        displayContent();
+    }
+
     // Change getCurrentUpdateInterval to long
     unsigned long updateInterval = getCurrentUpdateInterval();
 
@@ -109,6 +121,7 @@ void loop() {
 
     // Check if the button is pressed to switch display mode
     if (digitalRead(NEXT_BUTTON_PIN) == HIGH) {
+        lastButtonPressTime = currentTime;
         switchDisplayState();
         displayContent();
         delay(1000); // Debounce the button press
