46debacce494c8c764bdfed89b5d569ccf38e507
F.S. Koulen <feryllia.koulen@hva.nl>
2023-10-18 20:22:03 +0200

    Alter tunnel.sh to restart if localtunnel crashes

--- docker/images/tunnel/tunnel.sh
+++ docker/images/tunnel/tunnel.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# Tunnels the traffic from our nginx-docker container to the localtunnel server. Writes the output to a file.
-lt --local-host nginx --port $1 --subdomain $2 > /tmp/localtunnel &
+# Get the port and subdomain from the command line arguments.
+port=$1
+subdomain=$2
 
-# PHP creates a simple REST-server on port 8000 where we make the /tmp directory available.
-php -S 0.0.0.0:8000 -t /tmp
+# Tunnels the traffic from our nginx-docker container to the localtunnel server. Writes the output to a file.
+function localtunnel {
+  lt --local-host nginx --port "$1" --subdomain "$2" > /tmp/localtunnel &
+  # PHP creates a simple REST-server on port 8000 where we make the /tmp directory available.
+  php -S 0.0.0.0:8000 -t /tmp
+}
+
+
+until localtunnel "$port" "$subdomain"; do
+echo "localtunnel server crashed, restarting..."
+sleep 2
+done
